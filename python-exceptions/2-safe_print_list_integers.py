326fb0da8370782bf1b59f0ef1baab01a0b37f0b
reemmalyamani <alyamanireem@gmail.com>
2026-01-21 11:05:12 +0300

    adding exception tasks

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    i = 0
+
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+        except IndexError:
+            break
+        i += 1
+
+    print()
+    return count
+
